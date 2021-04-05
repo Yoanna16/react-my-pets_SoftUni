d58e2ae44589f66d05c5725fd54d88025157cac4
Yoanna Borisova <yoannaborisova@gmail.com>
2021-04-05 14:07:21 +0200

    My first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+
+import { Route, Switch } from 'react-router-dom';
+
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Categories from './components/Categories/Categories';
+import PetDetails from './components/PetDetails/PetDetails'
+
+
 import './App.css';
+import CategoryNavigation from './components/Categories/CategoryNavigation/CategoryNavigation';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header/>
+      <Switch>
+        <Route path="/" component={Categories} />
+        <Route path="/categories/:category" render={props => <Categories {...props} />} />
+        <Route path="/pets/details/:petId" exact render={props => <PetDetails {...props} />} />
+      </Switch>
+
+      <Footer/>
     </div>
   );
 }
